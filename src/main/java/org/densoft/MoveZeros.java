11803bbde0c9b7066594226a3091c843d69a7070
dennis githinji <wakahiad@gmail.com>
2023-11-12 22:24:53 +0300

    move zeros to end of array

--- src/main/java/org/densoft/MoveZeros.java
+++ src/main/java/org/densoft/MoveZeros.java
@@ -7,20 +7,24 @@ public class MoveZeros {
 
     public static void main(String[] args) {
         int[] nos = {3, 0, 9, 0, 7, 8, 0};
-
+        // Initialize an nonZeroIndex to track non-zero elements' positions
         int nonZeroIndex = 0;
-
+        // Iterate through the array
         for (int i = 0; i < nos.length; i++) {
+            // If the element is non-zero
             if (nos[i] != 0) {
+                // Place it at the current nonZeroIndex
                 nos[nonZeroIndex] = nos[i];
                 if (i != nonZeroIndex) {
-
-                    nos[i] = 0;
+                    // If i and nonZeroIndex are different, replace the current i with zero
                 }
-                nonZeroIndex++;
+                nos[i] = 0;
             }
+            // Move to the next nonZeroIndex position
+            nonZeroIndex++;
         }
         System.out.println(Arrays.toString(nos));
+
     }
 
 }
